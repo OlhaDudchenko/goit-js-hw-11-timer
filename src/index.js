e1a0953c695647903039cdd82bb07709791095a7
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-07-03 12:54:59 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -6,38 +6,35 @@ class CountdownTimer{
   constructor({ selector, targetDate }) {
     this.selector = selector;
     this.targetDate = targetDate;
-    this.timeContainer = document.querySelector(this.selector);
-    this.timerList = this.timeContainer.children;
-    this.days = this.timerList[0].firstElementChild;
-    this.hours = this.timerList[1].firstElementChild;
-    this.mins = this.timerList[2].firstElementChild;
-    this.secs = this.timerList[3].firstElementChild;
-  
     this.interval = null;
     this.start();
+    
    }
   start() {
-
-    this.interval=setInterval(() => {
-      const currentTime = Date.now();
-      const time = this.targetDate - currentTime;
-      this.getTimeComponent(time);
-      this.stop(time);
+   this.interval=setInterval(() => {
+    const currentTime = Date.now();
+    const time = this.targetDate - currentTime;
+    this.getTimeComponent(time);
     }, 1000);
 
 
   }
-  getTimeComponent(time) {
- 
-    this.days.innerHTML = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-    this.hours.innerHTML = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    this.mins.innerHTML = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    this.secs.innerHTML = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-   }
 
-  stop(time) {
+  getTimeComponent(time) {
+    const timeContainer = document.querySelector(this.selector);
+    const timerList = timeContainer.children;
+    const days = timerList[0].firstElementChild;
+    const hours = timerList[1].firstElementChild;
+    const mins = timerList[2].firstElementChild;
+    const secs = timerList[3].firstElementChild;
+     days.innerHTML = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+     hours.innerHTML = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+     mins.innerHTML = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+     secs.innerHTML = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+   }
+  
+stop(time) {
     if (time < 0) {
-      
       clearInterval(this.interval);
       this.timeContainer.innerHTML = "TIME EXPIRED";
     }
